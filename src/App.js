b463c9569c1428d065e1af3141306532b92a7741
William Martinez <william.matinez@genui.co>
2019-12-09 16:12:05 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const pages = [
+  {name:"Page 1", url: "/page1"},
+  {name:"Page 2", url: "/page2"},
+  {name:"Page 3", url: "/page3"}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header"></div>
+      <div className="container">
+      <div className="sidebar">
+        {pages.map(page => {
+          return <div className="sidebar-item" onClick={()=>fetchPage(page.url)}>{page.name}</div>
+        })}
+      </div>
+      <iframe id="page-loader" title="webpage" name="page-loader" className="iframe"></iframe>
+      </div>
     </div>
   );
+}
+
+function fetchPage(url){
+  fetch(`http://localhost:3000${url}`)
+  .then(res => res.text())
+  .then(data => {
+    console.log(data)
+    const iframe = document.getElementById('page-loader')
+    iframe.srcdoc = data
+  })
+  .catch(e => {
+    console.log(e)
+  })
 }
 
 export default App;
