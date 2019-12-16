e74a9ec68023d5bd89a66374c6537c8c0811d7be
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-12-16 10:54:28 -0800

    REFACTOR - useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Pages from './Pages'
 import Widgets from './Widgets';
 import './App.css';
 
 function App() {
+    const [src, setSrc] = useState()
     return (
       <div>
         <Widgets/>
         <div className="container">
           <div className="sidebar">
-            <Pages/>
+            <Pages setSrc={setSrc}/>
           </div>
-          <iframe id="page-loader" title="webpage" name="page-loader" className="iframe"/>
+          <iframe id="page-loader" title="webpage" name="page-loader" className="iframe" src={src}/>
         </div>
       </div>
     );
--- src/Pages.js
+++ src/Pages.js
@@ -12,24 +12,18 @@ const styles = {
   }
 }
 
-function Pages(){
+function Pages({setSrc}){
   return (
     <Menu mode="inline" style={styles.menu}>
       {pages.map(page => {
         return (
-        <Menu.Item key={page.name} onClick={()=>changeSrc(page.file)} style={styles.menuItem}>
+        <Menu.Item key={page.name} onClick={()=>setSrc(page.file)} style={styles.menuItem}>
           <Icon type="file" />
           <span>{page.name}</span>
         </Menu.Item>)
       })}
     </Menu>
   )
-}
-
-function changeSrc(file){
-  const url = `http://localhost:3000/${file}`
-  const iframe = document.getElementById('page-loader')
-    iframe.src = url
 }
 
 export default Pages
