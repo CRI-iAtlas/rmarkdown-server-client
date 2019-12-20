85d446ba24287061a2e3d4bb73a6ec045456085d
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-12-20 13:22:47 -0800

    CLEANUP - removing unused UI elements

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -10,17 +10,17 @@ const menuItemStyle = {
 }
 
 export const AppHeader = () => <Header className="app-header" style={{ backgroundColor: "#222" }}>
-  <div className="logo">CRI iAtlas Portal</div>
+  <div className="logo">RMarkdown Server Demo</div>
   <Menu
     theme="dark"
     mode="horizontal"
     defaultSelectedKeys={['2']}
     style={{ lineHeight: '64px', backgroundColor: "#222" }}
   >
-    <Menu.Item key="1"><Icon type="bar-chart" />Export</Menu.Item>
+    {/* <Menu.Item key="1"><Icon type="bar-chart" />Export</Menu.Item>
     <Menu.Item key="2"><Icon type="tool" />Tools</Menu.Item>
     <Menu.Item key="3"><Icon type="info-circle" />About</Menu.Item>
     <Menu.Item key="4"><Icon type="file-text" />Documentation</Menu.Item>
-    <Menu.Item key="5"><Icon type="link   V" />Resources</Menu.Item>
+    <Menu.Item key="5"><Icon type="link   V" />Resources</Menu.Item> */}
   </Menu>
 </Header>
